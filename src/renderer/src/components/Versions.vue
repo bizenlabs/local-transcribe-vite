2a0b6c794a6f9b134cd955b56f1ddbfc9d6fd088
Kapil Gidwani <kapil.gidwani@gmail.com>
2025-08-01 19:18:11 +0200

    remove bell icon

--- src/renderer/src/components/Versions.vue
+++ src/renderer/src/components/Versions.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
-import { BellIcon } from '@heroicons/vue/24/solid'
 
 const versions = reactive({ ...window.electron.process.versions })
 </script>
@@ -10,6 +9,5 @@ const versions = reactive({ ...window.electron.process.versions })
     <li class="electron-version">Electron v{{ versions.electron }}</li>
     <li class="chrome-version">Chromium v{{ versions.chrome }}</li>
     <li class="node-version">Node v{{ versions.node }}</li>
-    <BellIcon></BellIcon>
   </ul>
 </template>
